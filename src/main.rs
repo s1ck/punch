44ecee588b872e009cc77b52aaec86422d3f968e
Martin Junghanns <martin.junghanns@neo4j.com>
2020-11-20 14:40:49 +0100

    Add start, stop and list functionality

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,133 @@
+extern crate clap;
+
+use core::fmt;
+use fmt::Display;
+use std::error::Error;
+use std::io;
+
+use chrono::{Duration, Local, TimeZone};
+use serde::export::Formatter;
+
+use args::args;
+
+use crate::args::Action;
+use crate::data::{Data, load, store};
+
 mod args;
 mod data;
 
-extern crate clap;
-
-use crate::data::{load, store};
-use args::args;
-use std::io;
-
 fn main() -> Result<(), io::Error> {
-    let data = load()?;
-    let action = args();
-    println!("{:?}", action);
+    let mut data = load()?;
+
+    let result = match args() {
+        Action::START(tasks) => start(&mut data, tasks),
+        Action::STOP(tasks) => stop(&mut data, tasks),
+        Action::LIST => list(&data),
+    };
+
+    if let Err(err) = result {
+        eprintln!("{}", err);
+    }
+
     store(data)?;
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use assert_cmd::Command;
+fn start(data: &mut Data, tasks: Vec<String>) -> Result<(), PunchError> {
+    let existing_tasks = tasks
+        .iter()
+        .filter(|t| data.tasks.contains_key(*t))
+        .map(|t| t.clone())
+        .collect::<Vec<_>>();
 
-    #[test]
-    fn test_start() {
-        Command::cargo_bin(env!("CARGO_PKG_NAME"))
-            .unwrap()
-            .arg("in")
-            .arg("t1")
-            .arg("t2")
-            .assert()
-            .stdout("START([\"t1\", \"t2\"])\n");
+    if !existing_tasks.is_empty() {
+        return Err(PunchError::ExistingTasks(existing_tasks))
     }
 
-    #[test]
-    fn test_stop() {
-        Command::cargo_bin(env!("CARGO_PKG_NAME"))
-            .unwrap()
-            .arg("out")
-            .arg("t1")
-            .arg("t2")
-            .assert()
-            .stdout("STOP([\"t1\", \"t2\"])\n");
+    for task in tasks {
+        let dt = Local::now();
+        println!("Starting task `{}` at {}", task, dt);
+        data.tasks.insert(task, dt.timestamp());
     }
 
-    #[test]
-    fn test_list() {
-        Command::cargo_bin(env!("CARGO_PKG_NAME"))
-            .unwrap()
-            .arg("list")
-            .assert()
-            .stdout("LIST\n");
+    Ok(())
+}
+
+fn stop(data: &mut Data, tasks: Vec<String>) -> Result<(), PunchError> {
+    let missing_tasks = tasks
+        .iter()
+        .filter(|t| !data.tasks.contains_key(*t))
+        .map(|t| t.clone())
+        .collect::<Vec<_>>();
+
+    if !missing_tasks.is_empty() {
+        return Err(PunchError::MissingTasks(missing_tasks))
+    }
+
+    for task in tasks {
+        let timestamp = data.tasks.remove(&task).unwrap();
+        let duration = Local::now() - Local.timestamp(timestamp, 0);
+        println!(
+            "Stopping task `{}`, was running: {}",
+            task,
+            pretty_duration(&duration)
+        );
+    }
+
+    Ok(())
+}
+
+fn list(data: &Data) -> Result<(), PunchError> {
+    for (task, timestamp) in data.tasks.iter() {
+        let duration = Local::now() - Local.timestamp(*timestamp, 0);
+
+        println!(
+            "Task `{}` is running since: {}",
+            task,
+            pretty_duration(&duration)
+        );
+    }
+    Ok(())
+}
+
+fn pretty_duration(duration: &Duration) -> String {
+    let days = duration.num_days();
+    let hours = if duration.num_hours() >= 24 {
+        duration.num_hours() % 24
+    } else {
+        duration.num_hours()
+    };
+    let minutes = if duration.num_minutes() >= 60 {
+        duration.num_minutes() % 60
+    } else {
+        duration.num_minutes()
+    };
+
+    let seconds = if duration.num_seconds() >= 60 {
+        duration.num_seconds() % 60
+    } else {
+        duration.num_seconds()
+    };
+
+    format!(
+        "{} days, {} hours, {} minutes, {} seconds",
+        days, hours, minutes, seconds
+    )
+}
+
+#[derive(Debug)]
+enum PunchError {
+    ExistingTasks(Vec<String>),
+    MissingTasks(Vec<String>),
+}
+
+impl Error for PunchError {}
+
+impl Display for PunchError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            PunchError::ExistingTasks(tasks) => write!(f, "The following tasks already exist: {:?}", tasks),
+            PunchError::MissingTasks(tasks) => write!(f, "The following tasks do not exist: {:?}", tasks),
+        }
     }
 }
